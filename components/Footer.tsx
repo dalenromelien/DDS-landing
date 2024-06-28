1dffd2f8315375fd91859ced6f899befb82c3521
dromelie <dromelie@nyit.edu>
2024-06-27 23:46:40 -0400

    escaping apos

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer items-center p-4 bg-primary text-base-100">
       <aside className="items-center grid-flow-col">
-        <p>Dalen's Digital Services &copy; 2024 - All right reserved</p>
+        <p>Dalen&apos;s Digital Services &copy; 2024 - All right reserved</p>
         <p>Located at: insert virtual address</p>
       </aside>
       <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
