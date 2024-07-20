e4c791b7fc5401000677fa067fc1982b7dcf1f3a
dalenromelien <dalenm.romelien@gmail.com>
2024-07-20 18:53:28 -0400

    added virtual address to website

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer items-center p-4 bg-primary text-base-100">
       <aside className="items-center grid-flow-col">
         <p>Dalen&apos;s Digital Services &copy; 2024 - All right reserved</p>
-        <p>Located at: insert virtual address</p>
+        <p>Address: 15001 Kercheval Ave PMB 535 Grosse Pointe Park, MI 48230</p>
       </aside>
       <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         <p>Email: dalensdigitalservices@gmail.com</p>
